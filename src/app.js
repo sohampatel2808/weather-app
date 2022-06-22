c9b4b81b078338e94b8e4ad0186e77341abb164d
Soham Patel <soham.patel2808@gmail.com>
2022-06-22 12:57:02 +0530

    setup express and add endpoint

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 const geocode = require('../src/utils/geocode');
 const forecast = require('../src/utils/forecast');
+const express = require('express');
+const app = express();
 
-geocode('Mumbai', (error, { longitude, latitude, location } = {}) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+app.get('', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/weather', (req, res) => {
+  geocode('Mumbai', (error, { longitude, latitude, location } = {}) => {
+    if (error) return res.send(error);
+
+    forecast(longitude, latitude, (error, responseForecastData) => {
+      if (error) return res.send(error);
   
-  forecast(longitude, latitude, (error, responseForecastData) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-
-    console.log(responseForecastData);
+      return res.send(responseForecastData);
+    });
   });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
